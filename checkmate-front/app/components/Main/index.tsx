1139a53f6cff592c70abf24c5f723cdb1f1a5224
wyjee <wooyeon.jee@gmail.com>
2024-10-05 21:17:17 +0900

    feature(front): add openai translation request

--- checkmate-front/app/components/Main/index.tsx
+++ checkmate-front/app/components/Main/index.tsx
@@ -7,63 +7,52 @@ import Loader from './Loader';
 import './index.css';
 
 const configuration = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true
 };
 const openai = new OpenAI(configuration);
+const openaiModel = 'gpt-3.5-turbo'
 
 const Main = ()=> {
-    const [isAlready, setAlready] = useState(false);
     const [text, setText] = useState("");
-    // const [previousText, setPreviousText] = useState("");
+    const [fixedText, setFixedText] = useState("");
     const [words, setWords] = useState(0);
     const [loading, setLoading] = useState(false);
-    const [corrected, setCorrected] = useState(false);
-
-    const ping = async () => {
-        const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'user',
-                    content: 'Hello!',
-                },
-            ],
-        });
-
-        console.log(response._request_id);
+    const [corrected, setFixed] = useState(false);
+    const [language, setLanguage] = useState("en");
+    const langPairs: {
+        [key: string]: string
+    } = {
+        en: 'English',
+        kr: 'Korean'
     }
 
-    // const callOpenAi = async () => {
-    //     const res = await fetch("/api/openai", {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             name: text
-    //         }),
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
-    //         },
-    //     })
-    //     const data = await res.json();
-    //     const message = data.text;
-    //     let i = -1;
-    //
-    //     setText("");
-    //
-    //     const typingEffect = setInterval(() => {
-    //         setText((prevText) => prevText + message[i]);
-    //         i++;
-    //
-    //         if (i === message.length - 1) {
-    //             clearInterval(typingEffect);
-    //
-    //             setWords(countWords(text));
-    //             setCorrected(false);
-    //             setLoading(false);
-    //         }
-    //     }, 50);
-    // }
+    const callOpenAi = async () => {
+
+        const params = {
+            model: openaiModel,
+            messages: [
+                {
+                    "role": "system",
+                    "content": `You will be provided with statements, and your task is to convert them to standard ${langPairs[language]}.`
+                },
+                {
+                    "role": "user",
+                    "content": "She no went to the market."
+                }
+            ],
+            temperature: 0.7,
+            max_tokens: 64,
+            top_p: 1
+        }
+        // const res = await openai.chat.completions.create(params);
+        // const data = res.choices[0]?.message?.content;
+        // const message = data.text;
+
+        const message = "She did not go to the market."
+        setFixedText(message)
+
+    }
 
     const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setText(e.target.value);
@@ -85,47 +74,60 @@ const Main = ()=> {
     }
 
     const handleSubmit = () => {
-        setCorrected(true);
+        callOpenAi()
+        setFixed(true);
     }
 
     useEffect(() => {
         if (loading || !corrected) return;
-        ping(); // should be removed
 
         try {
             setLoading(true);
-            // setPreviousText(text);
         } catch (error) {
             return console.error(`[ERROR] ${error}`);
         }
     }, [corrected]);
 
     return (
-        <div>
-            <div className={'translation_box'}>
-                <textarea
-                    className={'textarea'}
-                    placeholder="내용을 입력해주세요."
-                    onChange={handleTextChange}
-                    value={text}
-                />
+        <div className={'flex_col'}>
+            <div className={'flex_row'}>
+                <div className={'translation_box left'}>
+                    <textarea
+                        className={'textarea'}
+                        placeholder="내용을 입력해주세요."
+                        onChange={handleTextChange}
+                        value={text}
+                    />
 
-                <div className="buttons">
-                    <span className={'left'}>글자 수: {words}</span>
-                    <div className={'right'}>
-                        <button type="button" onClick={handleInitialization}>
-                            입력창 초기화
-                        </button>
-                        <button type="button"
-                                disabled={!words}
-                                onClick={handleSubmit}
-                        >
-                            {!loading ? <span>첨삭</span> : <Loader/>}
-                        </button>
+                    <div className="buttons">
+                        <span className={'left'}>글자 수: {words}</span>
+                        <div className={'right'}>
+                            <button type="button" onClick={handleInitialization}>
+                                입력창 초기화
+                            </button>
+                            <button type="button"
+                                    disabled={!words}
+                                    onClick={handleSubmit}
+                            >
+                                {!loading ? <span>첨삭</span> : <Loader/>}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className={'result_box right'}>
+                    <textarea
+                        className={'textarea'}
+                        value={fixedText}
+                        readOnly={true}
+                    />
+                    <div className={'buttons flex_right'}>
                         <button onClick={handleSave}>저장</button>
                     </div>
                 </div>
             </div>
+
+            {/* HISTORY COMPONENT */}
+            <div className={'content'}></div>
         </div>
     )
 }
